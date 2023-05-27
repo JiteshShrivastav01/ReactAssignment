1a0778cb33932ae596137a33af3a4aaf8483bedc
JiteshShrivastav01 <Jitesh.cs01@gmail.com>
2023-05-27 20:57:23 +0530

    Assign1Task8

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import ExpenseItem from './component/ExpensesItem' ;
 function App() {
   return (
     <div>
-              <h2>Let's get started!</h2>
-      <h2>Hello Task1</h2>
       <ExpenseItem></ExpenseItem> 
     </div>
   );
--- src/component/ExpensesItem.js
+++ src/component/ExpensesItem.js
@@ -1,13 +1,34 @@
+// function ExpenseItem() {
+//     return (
+//         <div>
+//             <div><h3>Expense Items</h3></div>
+//             <div>
+//                 <ul><li>Food Rs 10</li>
+//                     <li>Petrol Rs 100</li>
+//                     <li>Movies Rs 200</li>
+//                     </ul>
+//             </div>
+//         </div>
+//     );
+// }
+
+import "./ExpenseItem.css";
+
 function ExpenseItem() {
-    return (
-        <div>
-            <div><h3>Expense Items</h3></div>
-            <div>
-                <ul><li>Food Rs 10</li>
-                    <li>Petrol Rs 100</li><li>Movies Rs 200</li></ul>
-            </div>
-        </div>
-    );
+  const expenseDate = new Date(2023, 4, 17);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 294.67;
+  const LocationOfExpenditure = "Pune"
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <h2>{LocationOfExpenditure}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;

a36166b06c74b535fe83e58aa93d1884718c1a3d
JiteshShrivastav01 <Jitesh.cs01@gmail.com>
2023-05-27 23:47:02 +0530

    Assign1Task9

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import ExpenseItem from './component/ExpensesItem' ;
+import ExpenseItem from './component/ExpensesItem';
 
 function App() {
+  const Expenses = [
+    { date: new Date(2023, 5, 24), title: 'Food', amount: 200, location: 'Pune' },
+    { date: new Date(2023, 5, 25), title: 'Movie', amount: 300, location: 'Mumbai' },
+    { date: new Date(2023, 5, 26), title: 'Petrol', amount: 500, location: 'Goa' }
+  ];
+
+  const expenseItems = Expenses.map((expense, index) => (
+    <ExpenseItem
+      key={index}
+      date={expense.date}
+      title={expense.title}
+      amount={expense.amount}
+      location={expense.location}
+    />
+  ));
+
   return (
     <div>
-      <ExpenseItem></ExpenseItem> 
+      {expenseItems}
     </div>
   );
 }
 
 export default App;
+

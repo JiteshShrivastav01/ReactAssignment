d7d534e4ba0eef88c52a3884fb0d5064ff883180
JiteshShrivastav01 <Jitesh.cs01@gmail.com>
2023-05-27 19:03:05 +0530

    task1

---
+++ src/component/ExpensesItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+    return (
+        <div>
+            <div><h3>Expense Items</h3></div>
+            <div>
+                <ul><li>Food Rs 10</li>
+                    <li>Petrol Rs 100</li><li>Movies Rs 200</li></ul>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;

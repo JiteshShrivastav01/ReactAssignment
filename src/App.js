c3db2a56400ae9737d7143813af5428531545060
JiteshShrivastav01 <Jitesh.cs01@gmail.com>
2023-05-27 19:22:54 +0530

    task22222

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ExpenseItem from './component/ExpensesItem' ;
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+              <h2>Let's get started!</h2>
       <h2>Hello Task1</h2>
       <ExpenseItem></ExpenseItem> 
     </div>

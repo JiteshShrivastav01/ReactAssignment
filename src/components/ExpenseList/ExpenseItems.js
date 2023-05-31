0d3b2f15d36dd559b5a60329f9fd8ca6ba4021f3
JiteshShrivastav01 <Jitesh.cs01@gmail.com>
2023-05-31 15:34:01 +0530

    Assign2Task2

--- src/components/ExpenseList/ExpenseItems.js
+++ src/components/ExpenseList/ExpenseItems.js
@@ -3,24 +3,30 @@ import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import Card from "../UI/Card";
 
+import react, {useState} from 'react';
+
 const ExpenseItems = (props) => {
-  const deleteExpense = (e) => {
 
-    let item = e.target.parentElement;
-    item.remove()
-
+  const btnStyle={width:'100px',margin:'5px',borderRadius:'10px',fonSize: '1.25rem',weight:700,
+  padding:'0.5rem 1.5rem'}
+  
+  const [amount , setAmount] = useState(props.amount)
+  const updateAmount = () => {
+    setAmount('100')
+    console.log(amount)
   }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{props.name}</h2>
         <ExpenseDetails
-          amount={props.amount}
+          amount={amount}
           LocationOfExpenditure={props.LocationOfExpenditure}
         ></ExpenseDetails>
       </div>
-      <button onClick={deleteExpense}>Delete Expense</button>
+      <button onClick={updateAmount} style={btnStyle}>Update Expense</button>
     </Card>
   );
 }

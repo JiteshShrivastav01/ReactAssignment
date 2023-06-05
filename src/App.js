491ffc5c87d9d7f4166bf3b319b2900f921ab08d
JiteshShrivastav01 <Jitesh.cs01@gmail.com>
2023-06-05 11:33:52 +0530

    Assign2Task11

--- src/App.js
+++ src/App.js
@@ -1,52 +1,61 @@
 
-import { useState } from "react";
+import React, { useState } from 'react';
 import Expenses from "./components/ExpenseList/Expenses";
-
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const dummyExpenses = [
+  {
+    id: "el",
+    title: "Food",
+    amount: 500,
+    date: new Date(2023, 5, 15),
+  },
+  {
+    id: "bl",
+    title: "Shopping",
+    amount: 5000,
+    date: new Date(2021, 10, 12),
+  },
+  {
+    id: "ml",
+    title: "Movie",
+    amount: 1000,
+    date: new Date(2020, 6, 13),
+  },
+  {
+    id: "cl",
+    title: "Tour",
+    amount: 20000,
+    date: new Date(2022, 4, 12),
+  },
+  {
+    id: "xl",
+    title: "Dinner",
+    amount: 1200,
+    date: new Date(2021, 8, 12)
+  },
+
+];
+
+
 const App = () => {
-  const expensesData = [
-    {
-      id: "el",
-      title: "Food",
-      amount: 500,
-      date: new Date(2023, 5, 15),
-    },
-    {
-      id: "bl",
-      title: "Shopping",
-      amount: 5000,
-      date: new Date(2023, 5, 22),
-    },
-    {
-      id: "ml",
-      title: "Movie",
-      amount: 1000,
-      date: new Date(2023, 5, 13),
-    },
-    {
-      id: "cl",
-      title: "Tour",
-      amount: 20000,
-      date: new Date(2022, 5, 12),  
-    }
-    
-  ];
+
+  const [expenses,setExpenses] = useState(dummyExpenses);
   
-  const [expenseList , setExpenseList] = useState(expensesData)
-  const addExpenseHandler = (newExpense) =>{
-    setExpenseList([...expenseList ,newExpense]);
-    console.log(newExpense,"in app.js new expenses");
+
+  const addExpenseHandler = expense => {
+    console.log("In App.js");
+    console.log(expense);
+
+    setExpenses(prevExpenses => {
+      return [expense, ...prevExpenses]
+    });
   }
 
   return (
     <div>
-      <NewExpense onAddExpense = {addExpenseHandler} />
-     
-      {/* {expenses.map((any) => (
-        <ExpenseItems data={any}></ExpenseItems>
-      ))} */}
-      <Expenses expenses={expenseList} />
+      <NewExpense onAddExpense={addExpenseHandler}/>
+      <Expenses items={expenses} />
       {/* return React.createElement('div',{},
   React.createElement(Expenses,{items:expenses})); react object code */}
     </div>
--- src/components/ExpenseList/Expenses.js
+++ src/components/ExpenseList/Expenses.js
@@ -1,31 +1,38 @@
 import "./Expenses.css";
 import ExpenseItems from "./ExpenseItems";
 import Card from "../UI/Card";
-
 import ExpensesFilter from "./ExpensesFilter";
 import { useState } from "react";
 
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState("2020");
+  const [filteredYear, setFilteredYear]=useState('2020');
 
-  const filterChangeHandler = (selectedYear) => {
+  const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear);
+
   };
+
+  const filteredExpenses = props.items.filter(expense=> {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  
   return (
     <Card className="expenses">
-        <ExpensesFilter
+      <ExpensesFilter
        selected={filteredYear} 
-       onChangeFilter={filterChangeHandler}/>
+       onChangeFilter={filterChangeHandler}
+    />
 
-      {props.expenses.map((expense) => (
-        <ExpenseItems
-          key ={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        ></ExpenseItems>
-      ))}
+    {filteredExpenses.map((expense) => (
+      <ExpenseItems
+        key = {expense.id}
+        title = {expense.title}
+        amount = {expense.amount}
+        date = {expense.date}
+        />
+    ))}
     </Card>
   );
-};
+}
 export default Expenses;
